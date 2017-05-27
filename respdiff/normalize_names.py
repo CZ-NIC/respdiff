aeb020cc9ccdac2f0b9c15fba0af4784f36b1f28
Petr Špaček <petr.spacek@nic.cz>
2017-05-27 13:21:24 +0200

    normalize_names: add helper to encode non-ASCII names in \123 encoding

---
+++ respdiff/normalize_names.py
@@ -0,0 +1,34 @@
+from pprint import pprint
+import string
+import sys
+
+allowed_bytes = (string.ascii_letters + string.digits + '.-_').encode('ascii')
+trans = {}
+for i in range(0, 256):
+    if i in allowed_bytes:
+        trans[i] = bytes(chr(i), encoding='ascii')
+    else:
+        trans[i] = ('\%03i' % i).encode('ascii')
+#pprint(trans)
+
+while True:
+    line = sys.stdin.buffer.readline()
+    if not line:
+        break
+    line = line[:-1]  # newline
+
+    typestart = line.rfind(b' ') + 1  # rightmost space
+    if not typestart:
+        continue  # no RR type!?
+    typetext = line[typestart:]
+    if not typetext:
+        continue
+
+    # normalize name
+    normalized = b''
+    for nb in line[:typestart-1]:
+        normalized += trans[nb]
+    sys.stdout.buffer.write(normalized)
+    sys.stdout.buffer.write(b' ')
+    sys.stdout.buffer.write(typetext)
+    sys.stdout.buffer.write(b'\n')
