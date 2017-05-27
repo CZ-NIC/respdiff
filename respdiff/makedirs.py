6277e911c291be4fe614cfb2a8f0a4c0c3eb62db
Petr Špaček <petr.spacek@nic.cz>
2017-05-27 13:21:00 +0200

    makedirs: create directories in parallel
    Text parsing and wire format generation is expensive so we move it to
    workes to utilize more CPUs.

--- respdiff/makedirs.py
+++ respdiff/makedirs.py
@@ -1,3 +1,5 @@
+import itertools
+import multiprocessing.pool as pool
 import errno
 import os
 import sys
@@ -5,23 +7,46 @@ import sys
 import makeq
 
 
-i = 1
-with open(sys.argv[1]) as qlist:
-    for line in qlist:
+def read_text():
+    i = 0
+    for line in sys.stdin:
         line = line.strip()
-        if i == 8000000:
-            break
+        if line:
+            i += 1
+            yield (i, line)
+            if i % 10000 == 0:
+                print(i)
+            #if i > 770000:
+            #    print(line)
 
-        dirname = '%07d' % i
-        qfilename = '%s/q.dns' % dirname
-        try:
-            os.mkdir(dirname)
-        except OSError as ex:
-            if not ex.errno == errno.EEXIST:
-                raise
-        qry = makeq.qfromtext(line.split())
-        if makeq.is_blacklisted(qry):
-            continue
-        with open(qfilename, 'wb') as qfile:
-            qfile.write(qry.to_wire())
-        i += 1
+def gen_qfile(args):
+    i, qtext = args
+    dirname = '%09d' % i
+    qfilename = '%s/q.dns' % dirname
+    try:
+        os.mkdir(dirname)
+    except OSError as ex:
+        if not ex.errno == errno.EEXIST:
+            raise
+    try:
+        qry = makeq.qfromtext(qtext.split())
+    except BaseException:
+        print('line malformed: %s' % qtext)
+        return
+    if makeq.is_blacklisted(qry):
+        return
+    with open(qfilename, 'wb') as qfile:
+        qfile.write(qry.to_wire())
+
+def main():
+    #qstream = itertools.islice(read_text(), 100000)
+    qstream = read_text()
+    #for i in map(gen_qfile, qstream):
+    #    pass
+    with pool.Pool() as p:
+        for i in p.imap_unordered(gen_qfile, qstream, chunksize=1000):
+            pass
+
+if __name__ == '__main__':
+    main()
+
