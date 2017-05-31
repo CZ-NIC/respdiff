98e924e825365dc30744ebc39d40a70477b41e34
Petr Špaček <petr.spacek@nic.cz>
2017-05-31 15:37:36 +0200

    respdiff: lmdb2file utility to extract answers from LMDB to binary files
    This gets handy when you need to inspect answers in wire format.

---
+++ respdiff/lmdb2file.py
@@ -0,0 +1,38 @@
+import pickle
+from pprint import pprint
+import os
+import sys
+
+import lmdb
+
+import lmdbcfg
+import msgdiff
+
+
+def read_blobs_lmdb(lenv, db, qid):
+    with lenv.begin(db) as txn:
+        blob = txn.get(qid)
+        assert blob
+        answers = pickle.loads(blob)
+        return answers
+
+
+def write_blobs(blob_dict, workdir):
+    for k, v in blob_dict.items():
+        filename = os.path.join(workdir, k)
+        with open(filename, 'wb') as outfile:
+            outfile.write(v)
+
+
+config = lmdbcfg.env_open.copy()
+config.update({
+    'path': sys.argv[1],
+    'readonly': True
+    })
+lenv = lmdb.Environment(**config)
+db = lenv.open_db(key=b'answers', **lmdbcfg.db_open, create=False)
+
+qid = str(int(sys.argv[2])).encode('ascii')
+blobs = read_blobs_lmdb(lenv, db, qid)
+write_blobs(blobs, sys.argv[3])
+lenv.close()
