eee27a71d212d4f75eb4c26549547706b9d8ea80
Petr Špaček <petr.spacek@nic.cz>
2017-06-16 12:15:27 +0200

    qprep: cleanup and preparation for user interface implementation

---
+++ respdiff/blacklist.py
@@ -0,0 +1,10 @@
+import dns.rdatatype
+
+def obj_blacklisted(msg):
+    """
+    Detect blacklisted DNS message objects.
+    """
+    if len(msg.question) >= 1:
+        if msg.question[0].rdtype == dns.rdatatype.ANY:
+            return True
+    return False
--- respdiff/qprep.py
+++ respdiff/qprep.py
@@ -1,45 +1,48 @@
 import multiprocessing.pool as pool
 import sys
 
+import dns.message
+import dns.rdatatype
 import lmdb
 
+import blacklist
 import dbhelper
-import qtext
 
 
-def read_text():
+def read_lines(instream):
+    """
+    Yield (line number, stripped line text). Skip empty lines.
+    """
     i = 0
-    for line in sys.stdin:
+    for line in instream:
         line = line.strip()
         if line:
             i += 1
             yield (i, line)
 
 
-def gen_q(qstr):
+def int_or_fromtext(value, fromtext):
     try:
-        qry = qtext.qfromtext(qstr.split())
-    except BaseException:
-        print('line malformed: %s' % qstr)
-        return
-    if qtext.is_blacklisted(qry):
-        return
-    return qry.to_wire()
+        return int(value)
+    except ValueError:
+        return fromtext(value)
 
 
-def write_lmdb(qid, wire):
+def q_fromtext(line):
     """
-    Worker: write query wire format into database
+    Convert line from <qname> <RR type> to DNS query in IN class.
+
+    Returns: DNS message object
+    Raises: ValueError or dns.exception.Exception on invalid input
     """
-    global env
-    global db
+    qname, qtype = line.rsplit(None, 1)
+    qname = dns.name.from_text(qname)
+    qtype = int_or_fromtext(qtype, dns.rdatatype.from_text)
+    return dns.message.make_query(qname, qtype, dns.rdataclass.IN,
+                                  want_dnssec=True, payload=4096)
 
-    key = dbhelper.qid2key(qid)
-    with env.begin(db, write=True) as txn:
-        txn.put(key, wire)
 
-
-def lmdb_init(envdir):
+def wrk_lmdb_init(envdir):
     """
     Worker: initialize LMDB env and open 'queries' database
     """
@@ -56,15 +59,34 @@ def lmdb_init(envdir):
     db = env.open_db(key=b'queries', **dbhelper.db_open)
 
 
-def gen_wrapper_lmdb(args):
-    qid, qstr = args
-    wire = gen_q(qstr)
-    write_lmdb(qid, wire)
+def wrk_lmdb_write(qid, wire):
+    """
+    Worker: write query wire format into database
+    """
+    global env
+    global db
+
+    key = dbhelper.qid2key(qid)
+    with env.begin(db, write=True) as txn:
+        txn.put(key, wire)
+
+
+def wrk_process_line(args):
+    """
+    Worker: parse input line and write (qid, wire formt) to LMDB queries DB
+
+    Skips over empty lines, raises for malformed inputs.
+    """
+    qid, line = args
+    msg = q_fromtext(line)
+    if not blacklist.obj_blacklisted(msg):
+        wrk_lmdb_write(qid, msg.to_wire())
+
 
 def main():
-    qstream = read_text()
-    with pool.Pool(initializer=lmdb_init, initargs=(sys.argv[1],)) as workers:
-        for _ in workers.imap_unordered(gen_wrapper_lmdb, qstream, chunksize=1000):
+    qstream = read_lines(sys.stdin)
+    with pool.Pool(initializer=wrk_lmdb_init, initargs=(sys.argv[1],)) as workers:
+        for _ in workers.imap_unordered(wrk_process_line, qstream, chunksize=1000):
             pass
 
 if __name__ == '__main__':
