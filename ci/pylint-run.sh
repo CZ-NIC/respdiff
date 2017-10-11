9b333637b34975d8000c3339fd2a78434aa030e7
Stepan Kotek <stepan.kotek@nic.cz>
2017-10-11 16:07:21 +0200

    improve pylint checks during CI

--- ci/pylint-run.sh
+++ ci/pylint-run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 WORKING_DIR=$(pwd);
 RET_CODE=0;
-for DIR in $(find . -type d | grep -v '\./\.' | grep -v '\./cache_usage_benchmark');
+for FILE in $(find . -name "*.py" | sort | grep -v '\./cache_usage_benchmark');
 do
   cd ${WORKING_DIR}
-  cd ${DIR}
-  find . -maxdepth 1 -type f  | grep  -q .py$ && \
-  printf "\n*Working directory: ${DIR}\n" && \
-  (python3 -m pylint -E `find . -maxdepth 1 -type f | grep .py$` && echo "No PYLINT errors detected in this directory" || RET_CODE=1)
+  cd `dirname ${FILE}`
+  echo ${FILE}
+  python3 -m pylint -E `basename ${FILE}` 2>/dev/null || RET_CODE=1
 done
+
 cd ${WORKING_DIR}
 exit $RET_CODE
