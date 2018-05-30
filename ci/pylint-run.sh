af61c76d389474d201a648b897b0a5ff624bdc0b
Tomas Krizek <tomas.krizek@nic.cz>
2018-05-30 13:58:15 +0200

    pylint: remove legacy subdir from script

--- ci/pylint-run.sh
+++ ci/pylint-run.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# Find Python modules and standalone Python scripts, skip directory
-# cache_usage_benchmark
+# Find Python modules and standalone Python scripts
 FILES=$(find . \
 	-type d -exec test -e '{}/__init__.py' \; -print -prune -o \
-	-path './cache_usage_benchmark' -prune -o \
 	-name '*.py' -print)
 
-PYTHONPATH=response_differences python3 -m pylint -j 2 --rcfile pylintrc ${FILES}
+python3 -m pylint -j 2 --rcfile pylintrc ${FILES}
