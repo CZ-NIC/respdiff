ddca4e2a49ab57723b5cf56a20909136495f95a4
Tomas Krizek <tomas.krizek@nic.cz>
2018-07-20 14:16:21 +0200

    job_manager: utility stat scripts

---
+++ contrib/job_manager/make_stats.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o nounset
+
+RESPDIFF_SRC=/var/opt/respdiff
+
+COMMIT_DIR=${1:-.}
+pushd ${COMMIT_DIR}
+
+ID=$(basename ${PWD})
+
+for DIR in *; do
+    if [ -d "${DIR}" ]; then
+        STATFILE=${ID}_${DIR}_stats.json
+        ${RESPDIFF_SRC}/sumstat.py -s "${STATFILE}" ${DIR}/*_report.json
+        ${RESPDIFF_SRC}/statcmp.py -s "${STATFILE}" -c "${DIR}/respdiff.cfg" -l "${ID}_${DIR}"
+
+        STATFILE=${ID}_${DIR}_stats.diffrepro.json
+        ${RESPDIFF_SRC}/sumstat.py -s "${STATFILE}" ${DIR}/*_report.diffrepro.json
+        ${RESPDIFF_SRC}/statcmp.py -s "${STATFILE}" -c "${DIR}/respdiff.cfg" -l "${ID}_${DIR}.diffrepro"
+    fi
+done
+
+popd
