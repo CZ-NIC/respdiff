d48661d2f15aecd3293504d295bd7001304b1ec2
Tomas Krizek <tomas.krizek@nic.cz>
2018-08-08 16:28:46 +0200

    fixup! WIP: add statcmp

--- statcmp.py
+++ statcmp.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-from typing import Optional, Sequence, Union
+import sys
+from typing import Optional, Sequence
 
 from respdiff import cli
 from respdiff.dataformat import Summary
@@ -16,20 +17,54 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt  # noqa
 
 
+COLOR_OK = 'tab:blue'
+COLOR_GOOD = 'tab:green'
+COLOR_BAD = 'xkcd:bright red'
+COLOR_BOUNDARY = 'tab:orange'
+COLOR_BG = 'tab:gray'
+COLOR_LABEL = 'black'
+
 VIOLIN_FIGSIZE = (3, 6)
-VIOLIN_COLOR = 'tab:grey'
 
 
-def plot_violin(
+def violin_plot(
             ax: matplotlib.axes.Axes,
             stats: Stats,
             label: str,
-            samples: Optional[Union[float, Sequence[float]]] = None
-        ) -> None:
+            samples: Optional[Sequence[float]] = None
+        ) -> bool:
     def draw_hline(y, color, width=0.7):
         xmin = (1 - width) / 2
         xmax = 1 - xmin
         ax.axhline(y, color=color, xmin=xmin, xmax=xmax)
+
+    below_min = False
+    above_max = False
+
+    if samples is not None:
+        below_min = any(sample < stats.min for sample in samples)
+        above_max = any(sample > stats.max for sample in samples)
+
+        # draw line representing sample(s)
+        for sample in samples:
+            if sample < stats.min:
+                color = COLOR_GOOD
+                below_min = True
+            elif sample <= stats.cutoff:
+                color = COLOR_OK
+            else:
+                color = COLOR_BAD
+                above_max = True
+            draw_hline(sample, color)
+
+    # draw colored title
+    if above_max:
+        color = COLOR_BAD
+    elif below_min:
+        color = COLOR_GOOD
+    else:
+        color = COLOR_LABEL
+    ax.set_title(label, fontdict={'fontsize': 14}, color=color)
 
     # turn off axis spines
     for sp in ['right', 'top', 'bottom']:
@@ -37,20 +72,16 @@ def plot_violin(
     # move the left ax spine to center
     ax.spines['left'].set_position(('data', 1))
 
-    ax.set_title(label, fontdict={'fontsize': 14})
-
     # plot graph
     violin_parts = ax.violinplot(stats.sequence, bw_method=0.07,
                                  showmedians=False, showextrema=False)
     for pc in violin_parts['bodies']:
-        pc.set_facecolor(VIOLIN_COLOR)
-        pc.set_edgecolor(VIOLIN_COLOR)
-    draw_hline(stats.min, VIOLIN_COLOR, 0.5)
-    draw_hline(stats.median, VIOLIN_COLOR, 0.5)
-    draw_hline(stats.max, VIOLIN_COLOR, 0.5)
-
-    cutoff = stats.cutoff
-    draw_hline(cutoff, 'tab:orange', 0.9)
+        pc.set_facecolor(COLOR_BG)
+        pc.set_edgecolor(COLOR_BG)
+    draw_hline(stats.min, COLOR_BG, 0.5)
+    draw_hline(stats.median, COLOR_BG, 0.5)
+    draw_hline(stats.max, COLOR_BG, 0.5)
+    draw_hline(stats.cutoff, COLOR_BOUNDARY, 0.9)
 
     # customize axis ticks
     ax.xaxis.set_major_locator(ticker.NullLocator())
@@ -63,59 +94,52 @@ def plot_violin(
     ax.yaxis.set_minor_locator(ticker.NullLocator())
     ax.tick_params(labelsize=14)
 
-    # draw line representing sample(s)
-    if samples is not None:
-        if not isinstance(samples, list):
-            samples = [samples]  # type: ignore
-        for sample in samples:
-            if sample < stats.min:
-                color = 'tab:green'
-            elif sample <= cutoff:
-                color = 'tab:blue'
-            else:
-                color = 'tab:red'
-            draw_hline(sample, color)
+    return not above_max
+
+
+def _axes_iter(axes, width: int):
+    index = 0
+    while True:
+        ix = index // width
+        iy = index % width
+        index += 1
+        try:
+            yield axes[ix, iy]
+        except IndexError:
+            return
 
 
 def overview_plot(
             sumstats: SummaryStatistics,
             fields: Sequence[FieldLabel],
-            samples: Optional[Union[Summary, Sequence[Summary]]] = None,
-            label: str = ''
-        ) -> None:
+            samples: Optional[Sequence[Summary]] = None,
+            label: str = 'fields_overview'
+        ) -> bool:
     if samples is None:
         samples = []
-    elif not isinstance(samples, list):
-        samples = [samples]  # type: ignore
+
+    passed = True
+    OVERVIEW_X_FIG = 7
+    OVERVIEW_Y_FIG = 3
 
     # prepare subplot axis
-    fig, axes = plt.subplots(3, 7, figsize=(7*3, 3*6))  # TODO un-hardcode
-
-    def axes_iter(axes):
-        index = 0
-        while True:
-            ix = index // 7
-            iy = index % 7
-            index += 1
-            try:
-                yield axes[ix, iy]
-            except IndexError:
-                return
-
-    ax_it = axes_iter(axes)
+    fig, axes = plt.subplots(
+        OVERVIEW_Y_FIG,
+        OVERVIEW_X_FIG,
+        figsize=(OVERVIEW_X_FIG*VIOLIN_FIGSIZE[0], OVERVIEW_Y_FIG*VIOLIN_FIGSIZE[1]))
+    ax_it = _axes_iter(axes, OVERVIEW_X_FIG)
 
     # target disagreements
     assert sumstats.target_disagreements is not None
-    plot_violin(
+    passed &= violin_plot(
         next(ax_it),
         sumstats.target_disagreements,
         'target_disagreements',
         [len(summary) for summary in samples])
 
-    # TODO remove copypasta
     # upstream unstable
     assert sumstats.upstream_unstable is not None
-    plot_violin(
+    passed &= violin_plot(
         next(ax_it),
         sumstats.upstream_unstable,
         'upstream_unstable',
@@ -123,7 +147,7 @@ def overview_plot(
 
     # not 100% reproducible
     assert sumstats.not_reproducible is not None
-    plot_violin(
+    passed &= violin_plot(
         next(ax_it),
         sumstats.not_reproducible,
         'not_reproducible',
@@ -133,7 +157,7 @@ def overview_plot(
     assert sumstats.fields is not None
     fcs = [summary.get_field_counters() for summary in samples]
     for field in fields:
-        plot_violin(
+        passed &= violin_plot(
             next(ax_it),
             sumstats.fields[field].total,
             field,
@@ -143,53 +167,33 @@ def overview_plot(
     for ax in ax_it:
         ax.set_visible(False)
 
+    # display sample size
+    fig.text(
+        0.95, 0.95,
+        'stat sample size: {}'.format(len(sumstats.target_disagreements.sequence)),
+        fontsize=18, color=COLOR_BG, ha='right', va='bottom', alpha=0.7)
+
     # save image
     plt.tight_layout()
     plt.subplots_adjust(top=0.9)
-    fig.suptitle('Fields {}'.format(label), fontsize=22)
-    plt.savefig('statplot_fields.svg')  # TODO: this forcibly overwrites - .bak + warning
+    fig.suptitle(label, fontsize=22)
+    plt.savefig('{}.svg'.format(label))
     plt.close()
 
-
-# TODO plot mismatches
-#     # get all kinds of mismatches for field
-#     mismatches = []
-#     for fc in fcs:
-#         mismatches.extend([mismatch for mismatch in fc[field]])
-#     mismatches = set(mismatches)
-#     for mismatch in mismatches:
-#         mdata = [fc[field][mismatch] for fc in fcs]
-#         plot_seq(mdata, fstats[str(mismatch.key)].median, fstats[str(mismatch.key)].mad,
-#                  '{}_{}'.format(field, mismatch.key))
+    return passed
 
 
 def main():
     cli.setup_logging()
-    # TODO fix this doc + create doc/statcmp.rst
-    parser = argparse.ArgumentParser(description='\n'.join([
-        "Compare or plot reports against statistical data",
-        "",
-        "  - comparison: Reports can be checked if the fall within threshold",
-        "                defined by the statistics file.",
-        "                Exit codes:",
-        "                  0: all reports pass, or none were provided",
-        "                  1: general error",
-        "                  3: some of the reports don't pass",
-        "",
-        "  - plot: Statistical data is plotted using violin graphs, representing",
-        "          the distribution of samples (histogram). Upper threshold is",
-        "          represented by orange. If report is provided, its position in",
-        "          dataset is represented by blue (within expected bounds),",
-        "          red (exceeded threshold) or green (below minimum). Multiple",
-        "          reports can be provided.",
-        "",
-        ]))
+    parser = argparse.ArgumentParser(
+        description=("Plot and compare reports against statistical data. "
+                     "Returns non-zero exit code if any upper threshold is exceeded."))
 
     cli.add_arg_stats(parser)
     cli.add_arg_report(parser)
     cli.add_arg_config(parser)
-    parser.add_argument('-p', '--plot', choices=['all', 'off', 'fail'], default='all',
-                        help="which plots to generate")  # TODO implement
+    parser.add_argument('-l', '--label', default='fields_overview',
+                        help='Set plot label. It is also used for the filename.')
 
     args = parser.parse_args()
     reports = [report for report in args.report if report is not None]
@@ -197,8 +201,12 @@ def main():
     sumstats = args.stats
     field_weights = args.cfg['report']['field_weights']
 
-    # TODO what if field is missing in stats?
-    overview_plot(sumstats, field_weights, summaries)
+    passed = overview_plot(sumstats, field_weights, summaries, args.label)
+
+    if not passed:
+        sys.exit(3)
+    else:
+        sys.exit(0)
 
 
 if __name__ == '__main__':
