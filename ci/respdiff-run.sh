2e5abfc219f6480486676bc844bbf96d6ae4ce6d
Tomas Krizek <tomas.krizek@nic.cz>
2018-08-21 18:10:46 +0200

    ci: execute sumstat and statcmp in respdiff toolchain

--- ci/respdiff-run.sh
+++ ci/respdiff-run.sh
@@ -3,12 +3,14 @@ set -o errexit -o nounset -o xtrace
 time wget https://gitlab.labs.nic.cz/knot/respdiff/snippets/238/raw?inline=false -O - | head -n 1000 > /tmp/queries.txt
 CONFIG="respdiff.cfg"
 
-rm -r /tmp/respdiff.db |:
+rm -r /tmp/respdiff.db ||:
 
 ./qprep.py /tmp/respdiff.db < /tmp/queries.txt
 time ./orchestrator.py /tmp/respdiff.db -c "${CONFIG}"
 time ./msgdiff.py /tmp/respdiff.db -c "${CONFIG}"
 ./diffrepro.py /tmp/respdiff.db -c "${CONFIG}"
 ./diffsum.py /tmp/respdiff.db -c "${CONFIG}"
+./sumstat.py /tmp/respdiff.db/report.json
+./statcmp /tmp/respdiff.db/report.json -l test
 
 # it must not explode/raise an unhandled exception
