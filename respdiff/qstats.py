1eeb384cd37f6567e9f29c8e907785efe2b83cf6
Tomas Krizek <tomas.krizek@nic.cz>
2018-09-04 18:39:00 +0200

    WIP: poc qstats evaluation

--- respdiff/qstats.py
+++ respdiff/qstats.py
@@ -1,13 +1,15 @@
 import collections
 from enum import Enum
+import logging
 from typing import Any, Mapping, Optional, Set, Sequence
 
-from .dataformat import JSONDataObject, DiffReport
+from .dataformat import DiffReport, JSONDataObject, QueryData
 from .typing import QID
 
 
 UPSTREAM_UNSTABLE_THRESHOLD = 0.1
 ALLOWED_FAIL_THRESHOLD = 0.05
+MAX_REPRO_ATTEMPTS = int(1 / ALLOWED_FAIL_THRESHOLD) + 1
 
 
 class QueryStatus(Enum):
@@ -16,14 +18,10 @@ class QueryStatus(Enum):
     FAILING = 3
 
 
-def get_query_status(
-            total: int,
-            target_disagrees: int,
-            others_disagree: int
-        ) -> QueryStatus:
-    if others_disagree / total >= UPSTREAM_UNSTABLE_THRESHOLD:
+def get_query_status(query_data: QueryData) -> QueryStatus:
+    if query_data.others_disagree / query_data.total >= UPSTREAM_UNSTABLE_THRESHOLD:
         return QueryStatus.UNKNOWN
-    if target_disagrees / total < ALLOWED_FAIL_THRESHOLD:
+    if query_data.target_disagrees / query_data.total < ALLOWED_FAIL_THRESHOLD:
         return QueryStatus.PASSING
     return QueryStatus.FAILING
 
@@ -46,16 +44,91 @@ class QueryStatistics(JSONDataObject):
         if _restore_dict is not None:
             self.restore(_restore_dict)
 
+    def add_query(self, qid: QID, query_data: QueryData) -> None:
+        status = get_query_status(query_data)
+        if status == QueryStatus.FAILING:
+            self.failing.add(qid)
+        elif status == QueryStatus.UNKNOWN:
+            self.unknown.add(qid)
+
+    def get_unseen_failures(
+                self,
+                report: DiffReport,
+                max_repro_attempts: int = MAX_REPRO_ATTEMPTS,
+                verified: bool = False
+            ) -> Set[QID]:
+        """Return new failures from a provided report (when compared to this set).
+
+        Failures can be either unverified, which need further reproducing,
+        or verified, when the maximum allowed failures (in relation to maximum repro
+        attempt) were exceeded during reproduction attempt."""
+        assert report.target_disagreements is not None
+        candidates = {
+            qid: report.get_query_data(qid) for qid in report.target_disagreements
+            if qid not in self.unknown}
+
+        # filter upstream unstable
+        max_upstream_unstable = int(max_repro_attempts * UPSTREAM_UNSTABLE_THRESHOLD)
+        candidates = {
+            qid: query_data for qid, query_data in candidates.items()
+            if query_data.others_disagree <= max_upstream_unstable}
+
+        max_allowed_failures = int(max_repro_attempts * ALLOWED_FAIL_THRESHOLD)
+        verified_failures = {
+            qid for qid, query_data in candidates.items()
+            if query_data.target_disagrees > max_allowed_failures}
+
+        if verified:
+            return verified_failures
+        return set(candidates.keys()) - verified_failures
+
+    def get_fixed_queries(
+                self,
+                report: DiffReport,
+                max_repro_attempts: int = MAX_REPRO_ATTEMPTS,
+                verified: bool = False
+            ) -> Set[QID]:
+        """Return newly passing queries from a provided report (when compared to this set).
+
+        New fixes can be either unverified, which need to be reproduced mutliple times to
+        verify them, or verified, once the maximum number of reproducer attempts is reached
+        and the query is still evaluated as passing."""
+        assert report.target_disagreements is not None
+        candidates = {
+            qid: report.get_query_data(qid) for qid
+            in self.failing - set(report.target_disagreements.keys())}
+
+        # filter upstream unstable
+        max_upstream_unstable = int(max_repro_attempts * UPSTREAM_UNSTABLE_THRESHOLD)
+        candidates = {
+            qid: query_data for qid, query_data in candidates.items()
+            if query_data.others_disagree <= max_upstream_unstable}
+
+        # filter failing
+        max_allowed_failures = int(max_repro_attempts * ALLOWED_FAIL_THRESHOLD)
+        candidates = {
+            qid: query_data for qid, query_data in candidates.items()
+            if query_data.target_disagrees <= max_allowed_failures}
+
+        min_required_passing = max_repro_attempts - max_allowed_failures
+        verified_fixed = {
+            qid for qid, query_data in candidates.items()
+            if query_data.total - query_data.target_disagrees >= min_required_passing}
+
+        if verified:
+            return verified_fixed
+        return set(candidates.keys()) - verified_fixed
+
     @staticmethod
     def from_reports(reports: Sequence[DiffReport]) -> 'QueryStatistics':
+        """Create query statistics from multiple reports - usually used as a reference"""
         others_disagree = collections.Counter()   # type: collections.Counter
         target_disagrees = collections.Counter()  # type: collections.Counter
 
         # collect query results
         for report in reports:
             if report.reprodata is not None:
-                raise NotImplementedError(
-                    "Query stability analysis doesn't support diffrepro data!")
+                logging.warning("reprodata ignored when creating query stability statistics")
             assert report.other_disagreements is not None
             assert report.target_disagreements is not None
             for qid in report.other_disagreements.queries:
@@ -69,9 +142,6 @@ class QueryStatistics(JSONDataObject):
         suspect_queries = set(others_disagree.keys())
         suspect_queries.update(target_disagrees.keys())
         for qid in suspect_queries:
-            status = get_query_status(total, target_disagrees[qid], others_disagree[qid])
-            if status == QueryStatus.FAILING:
-                query_statistics.failing.add(qid)
-            elif status == QueryStatus.UNKNOWN:
-                query_statistics.unknown.add(qid)
+            query_statistics.add_query(
+                qid, QueryData(total, others_disagree[qid], target_disagrees[qid]))
         return query_statistics
