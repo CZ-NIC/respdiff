18d81919688413ab1ffad495f42172b5717212a4
Tomas Krizek <tomas.krizek@nic.cz>
2018-09-06 13:49:06 +0200

    diffsum: add support to omit unstable/failing queries

--- diffsum.py
+++ diffsum.py
@@ -14,40 +14,6 @@ from respdiff.query import (
     convert_queries, get_printable_queries_format, get_query_iterator)
 
 
-DEFAULT_LIMIT = 10
-GLOBAL_STATS_FORMAT = '{:21s}   {:>8}'
-GLOBAL_STATS_PCT_FORMAT = '{:21s}   {:8d}   {:5.2f} % {:s}'
-
-
-def print_global_stats(report: DiffReport) -> None:
-    if report.total_answers is None or report.total_queries is None:
-        raise RuntimeError("Report doesn't contain sufficient data to print statistics!")
-    print('== Global statistics')
-    if report.duration is not None:
-        print(GLOBAL_STATS_FORMAT.format('duration', '{:d} s'.format(report.duration)))
-    print(GLOBAL_STATS_FORMAT.format('queries', report.total_queries))
-    print(GLOBAL_STATS_PCT_FORMAT.format(
-        'answers', report.total_answers,
-        report.total_answers * 100.0 / report.total_queries, 'of queries'))
-    print('')
-
-
-def print_differences_stats(summary: Summary, total_answers: int) -> None:
-    print('== Differences statistics')
-    print(GLOBAL_STATS_PCT_FORMAT.format(
-        'upstream unstable', summary.upstream_unstable,
-        summary.upstream_unstable * 100.0 / total_answers, 'of answers (ignoring)'))
-    if summary.not_reproducible:
-        print(GLOBAL_STATS_PCT_FORMAT.format(
-            'not 100% reproducible', summary.not_reproducible,
-            summary.not_reproducible * 100.0 / total_answers, 'of answers (ignoring)'))
-    print(GLOBAL_STATS_PCT_FORMAT.format(
-        'target disagrees', len(summary),
-        len(summary) * 100. / summary.usable_answers,
-        'of not ignored answers'))
-    print('')
-
-
 def main():
     cli.setup_logging()
     parser = argparse.ArgumentParser(
@@ -57,14 +23,23 @@ def main():
     cli.add_arg_config(parser)
     cli.add_arg_datafile(parser)
     cli.add_arg_limit(parser)
+    cli.add_arg_stats_filename(parser, default='')
     parser.add_argument('--without-diffrepro', action='store_true',
                         help='omit reproducibility data from summary')
+    parser.add_argument('--without-ref-unstable', action='store_true',
+                        help='omit unstable reference queries from summary')
+    parser.add_argument('--without-ref-failing', action='store_true',
+                        help='omit failing reference queries from summary')
 
     args = parser.parse_args()
     datafile = cli.get_datafile(args)
     report = DiffReport.from_json(datafile)
     field_weights = args.cfg['report']['field_weights']
 
+    if (args.without_ref_unstable or args.without_ref_failing) \
+            and not args.stats_filename:
+        logging.critical("Statistics file must be provided as a reference.")
+        sys.exit(1)
     if not report.total_answers:
         logging.error('No answers in DB!')
         sys.exit(1)
@@ -72,10 +47,23 @@ def main():
         logging.error('JSON report is missing diff data! Did you forget to run msgdiff?')
         sys.exit(1)
 
+    ignore_qids = set()
+    if args.without_ref_unstable or args.without_ref_failing:
+        try:
+            stats = cli.read_stats(args.stats_filename)
+        except ValueError as exc:
+            logging.critical(str(exc))
+            sys.exit(1)
+        if args.without_ref_unstable:
+            ignore_qids.update(stats.queries.unknown)
+        if args.without_ref_failing:
+            ignore_qids.update(stats.queries.failing)
+
     report = DiffReport.from_json(datafile)
     report.summary = Summary.from_report(
         report, field_weights,
-        without_diffrepro=args.without_diffrepro)
+        without_diffrepro=args.without_diffrepro,
+        ignore_qids=ignore_qids)
 
     cli.print_global_stats(report)
     cli.print_differences_stats(report)
