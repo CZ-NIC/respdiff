e2ea622ec19415fb2c81ae1fc91842033372d76a
Tomas Krizek <tomas.krizek@nic.cz>
2018-12-14 14:45:20 +0100

    contrib/dataset_filter: add lua filer to contrib

---
+++ contrib/dataset_filter/stats.lua
@@ -0,0 +1,43 @@
+-- Table to hold statistical functions
+local stats = {}
+
+-- Get the median of a table.
+function stats.median( t )
+  local temp = {}
+
+  -- deep copy table so that when we sort it, the original is unchanged
+  -- also weed out any non numbers
+  for k,v in pairs(t) do
+    if type(v) == 'number' then
+      table.insert( temp, v )
+    end
+  end
+
+  table.sort( temp )
+
+  -- If we have an even number of table elements or odd.
+  if math.fmod(#temp,2) == 0 then
+    -- return mean value of middle two elements
+    return ( temp[#temp/2] + temp[(#temp/2)+1] ) / 2
+  else
+    -- return middle element
+    return temp[math.ceil(#temp/2)]
+  end
+end
+
+-- Get the mean value of a table
+function stats.mean( t )
+  local sum = 0
+  local count= 0
+
+  for k,v in pairs(t) do
+    if type(v) == 'number' then
+      sum = sum + v
+      count = count + 1
+    end
+  end
+
+  return (sum / count)
+end
+
+return stats
