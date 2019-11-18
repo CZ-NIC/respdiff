049356e2c020ed098670e9bbde0fe60ad17c80c3
Tomas Krizek <tomas.krizek@nic.cz>
2019-11-18 19:20:57 +0100

    WIP: contrib/job_manager: set env variable for kresd

--- contrib/docker/knot-resolver/kresd.entrypoint.sh
+++ contrib/docker/knot-resolver/kresd.entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /var/opt/knot-resolver/.install/sbin/kresd -c /etc/knot-resolver/kresd.conf -v -f 1 /dev/shm >> /log/kresd.log
+exec env ASAN_OPTIONS="disable_coredump=0,abort_on_error=1" /var/opt/knot-resolver/.install/sbin/kresd -c /etc/knot-resolver/kresd.conf -v -f 1 /dev/shm >> /log/kresd.log
