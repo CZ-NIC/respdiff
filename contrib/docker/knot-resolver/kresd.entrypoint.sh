cf8add29699e35d608812d78c920d85969362c46
Tomas Krizek <tomas.krizek@nic.cz>
2019-11-18 16:06:58 +0100

    contrib/job_manager: kresd - generate coredump on ASAN error

--- contrib/docker/knot-resolver/kresd.entrypoint.sh
+++ contrib/docker/knot-resolver/kresd.entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export ASAN_OPTIONS=disable_coredump=0,abort_on_error=1
+
 exec /var/opt/knot-resolver/.install/sbin/kresd -c /etc/knot-resolver/kresd.conf -v -f 1 /dev/shm >> /log/kresd.log
