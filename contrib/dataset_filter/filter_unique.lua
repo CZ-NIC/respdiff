100e75cf0ec55834cb4d93476b8eb0a5a4bca56a
Petr Špaček <pspacek@isc.org>
2026-03-06 18:21:20 +0100

    WIP! DO NOT LOOK INSIDE!

--- contrib/dataset_filter/filter_unique.lua
+++ contrib/dataset_filter/filter_unique.lua
@@ -1,8 +1,19 @@
 #!/usr/bin/env dnsjit
+--local dbg = require 'debugger'
+--local pprint = require 'krprint'.pprint
+
 if #arg < 2 then
     print("usage: "..arg[1].." <pcap> [<pcap> ...]")
     return
 end
+
+
+
+local tlds = {}
+for line in io.lines("tlds") do
+    tlds[line] = true
+end
+
 
 local dn_limit = 5
 
@@ -23,16 +34,19 @@ local function add_unique(unique, qname, qtype, nlabels)
     if qname == nil or nlabels == nil then
         return
     end
+    qname = qname:lower()
     local reg_name = psl:registrable_domain(qname)
-    local _, count
     if reg_name == nil then
         reg_name = "."
-        count = 0
     else
-        _, count = string.gsub(reg_name, "%.", ".")
-        reg_name = string.lower(reg_name)  -- IN-aDDr.arPa. ->  in-addr.arpa.
+	-- rough heuristics, does not work with \. escaping, but we don't care
+	local _, _, tld = qname:find('([^.]+%.)$')
+	if tlds[tld] == nil then
+	    -- nonsense not present in the root zone
+	    return
+	end
     end
-    nlabels = nlabels - count
+--dbg()
 
     if unique[reg_name] == nil then
         unique[reg_name] = {}
@@ -44,22 +58,27 @@ local function add_unique(unique, qname, qtype, nlabels)
         unique[reg_name][nlabels][qtype] = {}
         unique[reg_name][nlabels][qtype]["#count"] = 0
     end
+    unique[reg_name]["#queries"] = (unique[reg_name]["#queries"] or 0) + 1
     if unique[reg_name][nlabels][qtype]["#count"] < dn_limit then
+        qname = string.lower(qname)
         if unique[reg_name][nlabels][qtype][qname] == nil then
-            unique[reg_name][nlabels][qtype][qname] = 1
+            unique[reg_name][nlabels][qtype][qname] = true
             unique[reg_name][nlabels][qtype]["#count"] = unique[reg_name][nlabels][qtype]["#count"] + 1
-            print(qname, reg_name, tonumber(nlabels), qtype)
+            unique[reg_name]["#outputs"] = (unique[reg_name]["#outputs"] or 0) + 1
+
+--print("NEW:", qname, pprint(unique, 'error'))
             return true
         end
     end
+--print("OLD:", qname, pprint(unique, 'error'))
     return false
 end
 
 local function process_packet(obj)
     dns.obj_prev = obj
     local ret = dns:parse_header()
-    if ret == 0 and dns.qr == 0 and dns.qdcount > 0 and dns:parse_q(q, labels, 127) == 0 then
-        local qnamestr, _ = label.tostring(dns, labels, 127)
+    if ret == 0 and dns.qr == 0 and dns.qdcount > 0 and dns:parse_q(q, labels, 127) == 0 and q.class == dns.CLASS.IN then
+        local qnamestr, _ = label.torfc1035(dns, labels, 127)
         return add_unique(unique, qnamestr, dns.type_tostring(q.type), tonumber(q.labels))
     end
     return false
@@ -68,19 +87,44 @@ end
 local function dn_counts(unique)
     local counts = {}
     -- extract unique[reg_name][nlabels][qtype]["#count"]
-    for _, t2 in pairs(unique) do  -- reg_name
-        for _, t3 in pairs(t2) do  -- nlabes
-            for key, t4 in pairs(t3) do  -- qtype
-                table.insert(counts, t4["#count"])
-            end
+    for reg_name, t2 in pairs(unique) do  -- reg_name
+        for nlabels, t3 in pairs(t2) do  -- nlabes
+	    if type(t3) == 'table' then
+		    for qtype, t4 in pairs(t3) do  -- qtype
+			table.insert(counts, t4["#count"])
+		    end
+	    end
         end
     end
     return counts
 end
 
+local function reg_counts(unique)
+    -- extract unique[reg_name][nlabels][qtype]["#count"]
+    for reg_name, t2 in pairs(unique) do  -- reg_name
+        if t2['#queries'] > 2 then
+            local weight_per_q = math.ceil(t2['#queries'] / t2['#outputs'])
+            for _, t3 in pairs(t2) do  -- nlabes
+                if type(t3) == 'table' then
+                    for qtype, t4 in pairs(t3) do  -- qtype
+                        for qname, istrue in pairs(t4) do  -- qtype
+                            if istrue == true then
+                                print(string.format("%s\t%s\t%d", qname, qtype, weight_per_q))
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+
 for n = 2, #arg do
     local pcap_input = input.new()
+    local pkts = 0
     pcap_input:open_offline(arg[n])
+    io.stderr:write(string.format("opened %s\n", arg[n]))
     local pcap_layer = layer.new()
     pcap_layer:producer(pcap_input)
     local producer, ctx = pcap_layer:produce()
@@ -88,6 +132,10 @@ for n = 2, #arg do
     while true do
         local obj = producer(ctx)
         if obj == nil then break end
+	pkts = pkts + 1
+	if pkts % 1000000 == 0 then
+            io.stderr:write(string.format("processed %.0f M pkts\n", pkts / 1e6))
+        end
         local pl = obj:cast()
         if obj:type() == "payload" and pl.len > 0 then
             process_packet(obj)
@@ -95,6 +143,8 @@ for n = 2, #arg do
     end
 end
 
+--print(pprint(unique, 'error'))
+reg_counts(unique)
 local counts = dn_counts(unique)
 
 io.stderr:write(string.format("median dn_limit: %.2f\n", stats.median(counts)))
